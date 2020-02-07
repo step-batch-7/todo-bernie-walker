bff6a2b53856b5af4408b36e10564830a4b5b8a5
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-02-07 17:39:28 +0530

    added Item class

---
+++ test/testItem.js
@@ -0,0 +1,36 @@
+const { assert } = require('chai');
+const { Item } = require('../lib/item');
+
+describe('Item', function() {
+  let item;
+
+  beforeEach(function() {
+    item = new Item('123', 'hello', false);
+  });
+
+  context('.mark', function() {
+    it('should mark done as true if it is already false', function() {
+      item.done = true;
+      item.mark();
+      assert.isFalse(item.done);
+    });
+
+    it('should mark done as false if it is already true', function() {
+      item.mark();
+      assert.isTrue(item.done);
+    });
+  });
+
+  context('.getId', function() {
+    it('should get the id of the give', function() {
+      assert.strictEqual(item.getId(), '123');
+    });
+  });
+
+  describe('.editTitle', function() {
+    it('should update the title with the given title', function() {
+      item.setTitle('world');
+      assert.strictEqual(item.title, 'world');
+    });
+  });
+});
